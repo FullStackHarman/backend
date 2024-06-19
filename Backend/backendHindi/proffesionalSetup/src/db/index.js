2ef3d5b650a97d51748d8a314b7f90e052a0fdcb
Harman Preet Singh <harmanprt5142@gmail.com>
2024-06-19 07:07:49 +0530

    database connected

---
+++ Backend/backendHindi/proffesionalSetup/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants';
+
+const connectDb = async ()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MongoDb_URI}/${DB_NAME}`;
+
+            console.log(`Connected Successfully To ${DB_NAME} Database at host ${connectionInstance.connection.host}`) }
+    catch (err){
+        console.log('Error connecting to Mongo DB', err);
+        process.exit(1);
+    }
+}
+export default connectDb;
